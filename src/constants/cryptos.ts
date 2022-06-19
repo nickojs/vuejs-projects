0527dea65e75e783937a8ecae83505817d26a149
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-19 00:49:09 -0300

    create constants

---
+++ src/constants/cryptos.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 'BTC',
+    value: 'Bitcoin',
+  },
+  {
+    id: 'ETH',
+    value: 'Ethereum',
+  },
+  {
+    id: 'XRP',
+    value: 'Ripple',
+  },
+];
---
+++ src/constants/currency.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 'BRL',
+    value: 'Brazilian Real',
+  },
+  {
+    id: 'USD',
+    value: 'US Dollar',
+  },
+  {
+    id: 'EUR',
+    value: 'Euro',
+  },
+];
