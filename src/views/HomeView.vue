c1631df20a3e303f7c62d83bda35f26df24ec479
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-19 15:13:18 -0300

    create base for data fetch and display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,97 @@
+<script lang="ts">
+/* eslint-disable vuejs-accessibility/form-control-has-label */
+import api from '@/services/api';
+import availableCryptos from '@/constants/cryptos';
+import availableCurrency from '@/constants/currency';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      availableCryptos,
+      availableCurrency,
+      selectedCrypto: null,
+      selectedCurrency: null,
+      isFetchable: false,
+      data: null,
+      loading: false,
+    };
+  },
+  methods: {
+    async fetchValues() {
+      if (this.isFetchable) {
+        this.loading = true;
+        try {
+          const result = await api({
+            params: {
+              ids: this.selectedCrypto,
+              convert: this.selectedCurrency,
+            },
+          });
+          this.data = result.data;
+        } catch (error) {
+          console.log(error);
+        } finally {
+          this.loading = false;
+        }
+      }
+    },
+  },
+  created() {
+    this.$watch(
+      () => ({
+        selectedCrypto: this.selectedCrypto,
+        selectedCurrency: this.selectedCurrency,
+      }),
+      (data: Record<string, null | string>) => {
+        if (data.selectedCrypto && data.selectedCurrency) {
+          this.isFetchable = true;
+        }
+      },
+    );
+  },
+  watch: {
+    isFetchable(newVal) {
+      if (newVal) {
+        this.fetchValues();
+      }
+    },
+  },
+});
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="wrapper">
+      <div class="currency-options">
+        <select v-model="selectedCrypto">
+          <option
+            v-for="crypto in availableCryptos"
+            :key="crypto.id"
+            :value="crypto.id"
+          >
+            {{ crypto.value }}
+          </option>
+        </select>
+
+        <p>To</p>
+
+        <select v-model="selectedCurrency">
+          <option
+            v-for="currency in availableCurrency"
+            :key="currency.id"
+            :value="currency.id"
+          >
+            {{ currency.value }}
+          </option>
+        </select>
+      </div>
+
+      <p v-if="loading">loading...</p>
+
+      <div v-if="data">
+        <p>{{ JSON.stringify(data) }}</p>
+      </div>
+    </div>
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
-</script>
